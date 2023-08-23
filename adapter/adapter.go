6e3a395dc05b9048b115c7a672508cd1aefa29ad
yaling888 <73897884+yaling888@users.noreply.github.com>
2023-08-24 05:00:00 +0800

    Fix: proxy dial err

--- adapter/adapter.go
+++ adapter/adapter.go
@@ -53,7 +53,7 @@ func (p *Proxy) Dial(metadata *C.Metadata) (C.Conn, error) {
 // DialContext implements C.ProxyAdapter
 func (p *Proxy) DialContext(ctx context.Context, metadata *C.Metadata, opts ...dialer.Option) (C.Conn, error) {
 	conn, err := p.ProxyAdapter.DialContext(ctx, metadata, opts...)
-	if !errors.Is(context.Canceled, err) {
+	if !errors.Is(err, context.Canceled) {
 		p.alive.Store(err == nil)
 	}
 	return conn, err
@@ -69,7 +69,7 @@ func (p *Proxy) DialUDP(metadata *C.Metadata) (C.PacketConn, error) {
 // ListenPacketContext implements C.ProxyAdapter
 func (p *Proxy) ListenPacketContext(ctx context.Context, metadata *C.Metadata, opts ...dialer.Option) (C.PacketConn, error) {
 	pc, err := p.ProxyAdapter.ListenPacketContext(ctx, metadata, opts...)
-	if !errors.Is(context.Canceled, err) {
+	if !errors.Is(err, context.Canceled) {
 		p.alive.Store(err == nil)
 	}
 	return pc, err
